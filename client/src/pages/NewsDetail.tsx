91242d746231906be862c3d805c961f8ca0c91d5
Nikola-Mircic <nikola123mircic@gmail.com>
2025-09-03 01:23:30 +0200

    Sklonjena stara funkcija za generisanje vesti

--- client/src/pages/NewsDetail.tsx
+++ client/src/pages/NewsDetail.tsx
@@ -3,8 +3,8 @@ import { useParams } from "react-router-dom";
 import NewsCard from "../components/NewsCard";
 import Comment from "../components/Comment";
 import AddComment from "../components/AddComment";
-import { getAllNews } from "../services/NewsService";
-import type { News } from "../services/NewsService";
+import type { VestDto } from "../models/vesti/VestDto";
+import { vestiApi } from "../api_services/vesti/VestAPIService";
 
 interface CommentType {
   username: string;
@@ -13,36 +13,46 @@ interface CommentType {
 
 const NewsDetail: React.FC = () => {
   const { id } = useParams<{ id: string }>();
-  const [news, setNews] = useState<News | null>(null);
-  const [relatedNews, setRelatedNews] = useState<News[]>([]);
+  const [news, setNews] = useState<VestDto | null>(null);
+  const [relatedNews, setRelatedNews] = useState<VestDto[]>([]);
   const [comments, setComments] = useState<CommentType[]>([
     { username: "Ana", content: "Odlična vest!" },
     { username: "Marko", content: "Hvala za info." },
   ]);
 
   useEffect(() => {
-    if (id) {
-      const allNews = getAllNews();
-      const found = allNews.find((n) => n.id === Number(id)) || null;
-      setNews(found);
+    (async () => {
+			if (id) {
+				// OVO JE SAMO PRIMER!
+				var found = await vestiApi
+					.getById(parseInt(id))
+					.then((res) => res.data);
 
-      // Povezane vesti - jednostavno po prvih 3 koje nisu trenutna
-      setRelatedNews(allNews.filter((n) => n.id !== Number(id)).slice(0, 3));
-    }
+				if (!found) setNews(null);
+				else setNews(found);
+
+				// Povezane vesti - jednostavno po prvih 3 koje nisu trenutna
+				var slicne = await vestiApi
+					.getSlicneVesti(Number(id))
+					.then((res) => res.data);
+				if (!slicne) setRelatedNews([]);
+				else setRelatedNews(slicne);
+			}
+		})();
   }, [id]);
 
   if (!news) return <p>Učitavanje vesti...</p>;
 
   return (
-    <div className="p-4 max-w-4xl mx-auto">
-      <h1 className="text-3xl font-bold mb-2">{news.title}</h1>
-      <p className="text-gray-500 mb-4">Pregleda: {news.views}</p>
-      <p className="mb-4">{news.content}</p>
+    <div className="p-4 max-w-4xl mx-auto ">
+      <h1 className="text-3xl font-bold mb-2">{news.naslov}</h1>
+      <p className="text-gray-500 mb-4">Pregleda: {news.brPregleda}</p>
+      <p className="mb-4">{news.tekst}</p>
 
       <h2 className="text-2xl font-semibold mt-6 mb-2">Povezane vesti</h2>
       <div className="grid grid-cols-1 md:grid-cols-3 gap-4">
         {relatedNews.map((n) => (
-          <NewsCard key={n.id} id={n.id} title={n.title} views={n.views} />
+          <NewsCard key={n.id} id={n.id} title={n.naslov} views={n.brPregleda} />
         ))}
       </div>
 
--- client/src/pages/NewsFeed.tsx
+++ client/src/pages/NewsFeed.tsx
@@ -1,25 +1,27 @@
-import { useEffect, useState } from "react";
+import { vestiApi } from "../api_services/vesti/VestAPIService";
 import NewsCard from "../components/NewsCard";
-import { getAllNews } from "../services/NewsService";
-import type { News } from "../services/NewsService";
 import useFetch from "../hooks/useFetch";
-
-
+import type { VestDto } from "../models/vesti/VestDto";
+import type { ApiResponse } from "../types/common/ApiResponse";
 
 const NewsFeed = () => {
-  const {data: news, loading, error} = useFetch<News[]>(getAllNews);
+  const {data, loading, error} = useFetch<ApiResponse<VestDto[]> | undefined>(() => vestiApi.getNajpopularnije(0, 4));
 
 
   if(loading) return <div>Loading...</div>
   if(error) return <div>Error: {error.message}</div>
-  if(!news) return <div>No news available.</div>
+  if(!data) return <div>No news available.</div>
+
+  var news = data.data;
+  if(!news)
+    return <div>No data returned!</div>
 
   console.log(news);
 
   return (
 		<div className="p-4 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
 			{news.map((n) => (
-				<NewsCard key={n.id} id={n.id} title={n.naslov} views={n.br_pregleda} />
+				<NewsCard key={n.id} id={n.id} title={n.naslov} views={n.brPregleda} />
 			))}
 		</div>
 	);
