55ccea21a8c912434c6472826a986c4870cc0388
Nikola-Mircic <nikola123mircic@gmail.com>
2025-09-08 10:32:56 +0200

    OVo je finalni komit

--- client/src/pages/Profile.tsx
+++ client/src/pages/Profile.tsx
@@ -63,15 +63,6 @@ export default function ProfilePage() {
 					<span className="text-gray-500">Uloga:</span> {me.uloga}
 				</div>
 			</div>
-
-			<div className="mt-6">
-				<Link
-					to="/profile/edit"
-					className="inline-block bg-black text-white px-4 py-2 rounded"
-				>
-					Izmeni profil
-				</Link>
-			</div>
 		</div>
 	);
 }
